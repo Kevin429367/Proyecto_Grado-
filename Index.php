04c56cbab4a9dc014a1daeb73c9e6c99a5de1793
Kevin429367 <guerrerovasquez.19@gmail.com>
2025-11-19 22:56:24 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html> 
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Estilos/Principal.css">
+    <title>Página Informativa</title>
+    <link rel="stylesheet" href="Estilos/Principal.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+</head>
+<body> 
+  <header>
+        <h1>Mazorcas Mary</h1>
+        <nav>
+            <ul>
+                <li><a href="Productos/Productos.php">Productos</a></li>
+                <li><a href="Pedidos/Pedidos.php"><img src="Img/icons8-carrito-de-compras-50.png" alt="Carrito" class="menu-icon"></a></li>
+                <li><a href="Login/Login.html">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <div class="logo-container">
+        <img src="Img/Logo.png" alt="Logo" class="logo"> 
+    </div>
+    <main>
+        <section>
+            <h2>Acerca de Nosotros</h2>
+            <p>Somos una empresa ubicada en la central de Corabastos SA, distribuidores mayoristas de mazorcas en variedad de presentaciones. Llevamos 12 años llevando calidad desde el campo a los hogares, fruver y centrales de Colombia.</p>
+        </section>
+        <section class="map-container">
+            <h2>Puedes encontrarnos en la siguiente ubicación</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127257.4942161723!2d-74.31167830273438!3d4.630329200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9e832c0c7055%3A0xcbd1a812e6c3912d!2sPlaza%20de%20Mercado%20Corabastos!5e0!3m2!1ses!2sco!4v1727550537198!5m2!1ses!2sco" 
+                width="600" 
+                height="450" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy"></iframe>
+        </section>
+        <br>
+        <h2>La receta mágica junto con CocaCola</h2>
+        <br>
+        <iframe 
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02Ab6pq8mqMv8dcGWeC8B9X1SXqmXfhXJH4pn5zFQ4JBV5ZAXJt8Mxe4tbF8z5D9JAl%26id%3D100013069179611&show_text=true&width=500"
+        class="facebook-post"
+        width="500" 
+        height="1144"
+        style="border:none;overflow:hidden" 
+        scrolling="no" 
+        frameborder="0" 
+        allowfullscreen="true" 
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+    </iframe>
+    <br>
+        <section class="map-container">
+        <h2>La mejor calidad y precios del mercado</h2> 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/tElZDHIf_QE?si=qv61n07bRxRk-owK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </section>       
+    </main> 
+</body>
+<footer class="footer">
+    <div class="footer-bottom"> 
+      <p> 2025 <strong>S&M</strong> - Todos los Derechos Reservados.</p>
+    </div>
+  </footer>
+</html> 
+
